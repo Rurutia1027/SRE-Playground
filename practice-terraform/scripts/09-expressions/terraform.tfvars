6cd7674e1076b3b15ffe3068085d899336530fb6
Rurutia1027 <rurutia1027@gmail.com>
2025-01-10 20:44:21 +0800

    update map iterate operation and it notes

--- practice-terraform/scripts/09-expressions/terraform.tfvars
+++ practice-terraform/scripts/09-expressions/terraform.tfvars
@@ -14,3 +14,12 @@ objects_list = [
     lastname  = "Muller"
   }
 ]
+
+numbers_map = {
+  one   = 1
+  two   = 2
+  three = 3
+  four  = 4
+  five  = 5
+
+}
--- practice-terraform/scripts/09-expressions/variables.tf
+++ practice-terraform/scripts/09-expressions/variables.tf
@@ -2,9 +2,9 @@ variable "numbers_list" {
   type = list(number)
 }
 
-# variable "numbers_map" {
-#   type = map(number)
-# }
+variable "numbers_map" {
+  type = map(number)
+}
 
 variable "objects_list" {
   type = list(object({
