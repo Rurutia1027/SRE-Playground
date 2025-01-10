e9043fceecc48c00042029f115c63bec4cef3872
Rurutia1027 <rurutia1027@gmail.com>
2025-01-10 18:12:36 +0800

    add for list operations in .tf file

---
+++ practice-terraform/scripts/09-expressions/locals.tf
@@ -0,0 +1,15 @@
+locals {
+  math    = 2 * 2 - 2 * 1 + 1/ 5
+  equality = 2 == 3 # equality operators: == ; !=
+  comparison = 2 <= 3 # >=, <= , <, >
+  logical = false || true && true # true, false
+}
+
+output "operator_rets" {
+  value = {
+    ret_math       = local.math
+    ret_equality   = local.equality
+    ret_comparison = local.comparison
+    ret_logical    = local.logical
+  }
+}
