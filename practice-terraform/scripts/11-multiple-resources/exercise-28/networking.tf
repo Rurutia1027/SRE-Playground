79c3ea289d7be1eed2408a77da9f6968f589bb23
Rurutia1027 <rurutia1027@gmail.com>
2025-01-12 21:21:04 +0800

    update scripts for both creating 1 vpc, 2 subnet and 4 instances in which the instances subnet fields are assiend by 2 subnet

--- practice-terraform/scripts/11-multiple-resources/exercise-28/networking.tf
+++ practice-terraform/scripts/11-multiple-resources/exercise-28/networking.tf
@@ -13,5 +13,12 @@ resource "aws_vpc" "main" {
 
 # subnets declaration
 resource "aws_subnet" "main" {
-  count = var
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+
+  tags = {
+    Project = local.project
+    Name    = "${local.project}-${count.index}"
+  }
 }
