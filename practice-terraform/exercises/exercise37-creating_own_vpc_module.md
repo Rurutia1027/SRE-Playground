675d7f412009f997f30c9205d906cb491df5baac
Rurutia1027 <rurutia1027@gmail.com>
2025-01-13 21:44:05 +0800

    commit scripts for applying both vpc and ec2 instances via terraform aws module

---
+++ practice-terraform/exercises/exercise37-creating_own_vpc_module.md
@@ -0,0 +1,15 @@
+# Creating Our First VPC Module
+
+## Introduction
+
+In this exercise, we will start creating our first Virtual Private Cloud (VPC) module using Terraform.
+
+
+--- 
+
+## Desired Outcome
+
+--- 
+
+## Step-by-Step Guide 
+
--- practice-terraform/scripts/12-public-modules/exercise-36/networking.tf
+++ practice-terraform/scripts/12-public-modules/exercise-36/networking.tf
@@ -6,11 +6,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.16.0"
 
-  cidr = "10.0.0.0/16"
-  name = "12-public-modules-exercise35"
-  azs  = data.aws_availability_zones.azs.names
+  cidr            = "10.0.0.0/16"
+  name            = "12-public-modules-exercise35"
+  azs             = data.aws_availability_zones.azs.names
   private_subnets = ["10.0.0.0/24"]
-  public_subnets = ["10.0.128.0/24"]
+  public_subnets  = ["10.0.128.0/24"]
+
+  tags = local.common_tags
 }
 
 
