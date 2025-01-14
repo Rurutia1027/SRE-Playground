3f106a58f688aed112a86e433180329cdbb5fdcd
Rurutia1027 <rurutia1027@gmail.com>
2025-01-14 18:35:06 +0800

    update codes for exercise 40 in which support subnet config init via module

---
+++ practice-terraform/scripts/13-local-modules/exercise40/modules/networking/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_cidr_content" {
+  value = var.vpc_cidr
+}
+
+output "vpc_config" {
+  value = {
+    name       = var.vpc_config.name
+    cidr_block = var.vpc_config.cidr_block
+  }
+}
+
+output "subnet_config_output" {
+  value       = var.subnet_config
+  description = "The configuration of the subnets, including CIDR blocks and AZs"
+}
